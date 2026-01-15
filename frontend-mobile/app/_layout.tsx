2f32bd346a317103bb58523089103c86be877ebf
CarlosNunek <chnunez@uce.edu.ec>
2026-01-14 22:46:57 -0500

    Actualice frontend-mobile/app/_layout.tsx

--- frontend-mobile/app/_layout.tsx
+++ frontend-mobile/app/_layout.tsx
@@ -1,6 +1,9 @@
-// _layout.tsx
-//AuthProvider wrapper + Stack Navigation correcta
-
+/*Define el layout raíz de toda la app móvil:
+   Envuelve la navegación dentro de AuthProvider para que el estado de autenticación esté disponible globalmente.
+     Configura un Stack sin header visible.
+       Registra los grupos de rutas (auth) y (chat) y también la ruta de fallback +not-found.
+         Es el punto central de navegación y contexto de autenticación.
+*/
 import { Stack } from 'expo-router';
 import { AuthProvider } from '@/src/contexts/AuthContext';
 
