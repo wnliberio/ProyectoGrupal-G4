86c39f1a5e36341220a0396bc2b350ae9a0e2c40
CarlosNunek <chnunez@uce.edu.ec>
2026-01-14 22:25:53 -0500

    Actualice frontend-mobile/app/(chat)/_layout.tsx

--- frontend-mobile/app/(chat)/_layout.tsx
+++ frontend-mobile/app/(chat)/_layout.tsx
@@ -1,3 +1,8 @@
+// app/(chat)/_layout.tsx
+// _layout.tsx
+/*Define el layout de navegación del grupo (chat) usando expo-router. 
+   Crea un Stack sin header y registra las pantallas index y chat dentro del flujo de chat.
+*/
 import { Stack } from 'expo-router';
 
 export default function ChatLayout() {
